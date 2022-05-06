5e40c7c5cbd0190cc86fbdf656ca4234ed9c13d5
Your Name <sam@google.com>
2022-05-06 18:43:41 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import image from "./image/image.jpg";
+
+
+
+
+class App extends React.Component {
+  state = {
+    fullName: "",
+    bio: " ",
+    profession: "",
+    src: image,
+  };
+  handleClick=()=>this.setState({fullName:'Safa bhy',
+bio: ' web developper', profession: 'work',})
+  render() {
+    return(
+      <div className='conatiner'>
+      <h1> {this.state.fullName}</h1>
+      <h1> {this.state.bio}</h1>
+      <h1> {this.state.profession}</h1>
+     <img src={this.state.src} width="500" height="500" className='img'/>
+     <br/>
+      <button onClick={this.handleClick} className="btn"> ClickMe</button>
+      </div>
+    )
+  }
 }
 
 export default App;
